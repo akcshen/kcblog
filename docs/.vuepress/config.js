63839d84c37af0dc0cd7ed78d5f733676d31822b
kc <kcqingfeng@gmail.com>
2024-06-18 19:08:53 +0800

    feat:增加备案信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -120,7 +120,7 @@ module.exports = {
       // 页脚信息
       createYear: 2019, // 博客创建年份
       copyrightInfo:
-        'kc shen | MIT License <b/>\n <a href="https://beian.miit.gov.cn/">豫ICP备19035663号-2</a>', // 博客版权信息，支持a标签
+        'kc shen | MIT License <b/>\n <a href="https://beian.miit.gov.cn/">豫ICP备2024074563号</a>', // 博客版权信息，支持a标签
     },
     // 站点配置（首页 & 文章页）
     blogInfo: {
